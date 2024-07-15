916ae02f929eb198431a4da95f7cf13bfb00e0a6
MasterTrainerPK <perrylovesbooks@gmail.com>
2024-07-15 14:24:18 +0000

    Split display
    added display file
    added test code
    cleaned up comments
    added display docs

---
+++ display.py
@@ -0,0 +1,50 @@
+"""Handles the display and rendering of tiles
+contains the main render function"""
+import pygame
+window = None
+SCREEN_TILE_WIDTH = 64
+SCREEN_TILE_HEIGHT = 36
+# Although should be const, is not initialized untill the window is
+TILE_WIDTH = 0
+TILE_HEIGHT = 0
+
+tile_screen = []
+
+
+def to_screen_space(coord):
+    """converts a screen_tilespace tuple to screenspace tuple"""
+    return (coord[0] * TILE_WIDTH,
+            coord[1] * TILE_HEIGHT)
+
+
+def to_screen_tile_space(coord):
+    """converts a screenspace coord to a the screen_tilespace coord"""
+    return (int(coord[0] / TILE_WIDTH),
+            int(coord[1] / TILE_HEIGHT))
+
+
+def draw_tiles(surface):
+    """renders the tiles found in tile_screen"""
+    for i in range(SCREEN_TILE_WIDTH):
+        for j in range(SCREEN_TILE_HEIGHT):
+            surface.blit(tile_screen[i][j], to_screen_space((i, j)))
+
+
+def init():
+    """initializes the diplay call before accessing anything else"""
+    global window, TILE_WIDTH, TILE_HEIGHT
+    window = pygame.display.set_mode(pygame.display.get_desktop_sizes()[0])
+    TILE_WIDTH = window.get_width() / SCREEN_TILE_WIDTH
+    TILE_HEIGHT = window.get_height() / SCREEN_TILE_HEIGHT
+    tile_screen = []
+    for i in range(SCREEN_TILE_WIDTH):
+        tile_screen.append([])
+        for j in range(SCREEN_TILE_HEIGHT):
+            tile_screen[i].append(None)
+
+
+def render():
+    """Main game render function. renders entire frame"""
+    window.fill("purple")
+    draw_tiles(window)
+    pygame.display.flip()
--- main.py
+++ main.py
@@ -1,58 +1,47 @@
+"""The main gameloop
+this module should not be imported anywhere
+"""
 import pygame
-
+import display
 
 # pygame setup
 pygame.init()
+display.init()
 # TODO: Make this a little smarter...
-window = pygame.display.set_mode(pygame.display.get_desktop_sizes()[0])
 clock = pygame.time.Clock()
 running = True
 
 # asset setup
 tiles = []
-SCREEN_TILE_WIDTH = 80  # 640 / 8
-SCREEN_TILE_HEIGHT = 45  # 360 / 8
-TILE_WIDTH = window.get_width() / SCREEN_TILE_WIDTH
-TILE_HEIGHT = window.get_height() / SCREEN_TILE_HEIGHT
 missing_texture = pygame.image.load("image.png")
 missing_texture.convert()
 missing_texture = pygame.transform.scale(
                     missing_texture,
-                    (TILE_WIDTH, TILE_HEIGHT))
-tiles.append(missing_texture)
+                    (display.TILE_WIDTH, display.TILE_HEIGHT))
+brown_texture = pygame.image.load("Brown.png")
+brown_texture.convert()
+brown_texture = pygame.transform.scale(
+                    brown_texture,
+                    (display.TILE_WIDTH, display.TILE_HEIGHT))
+tiles.append(brown_texture)
 
 # might want to use arrays instead of lists
-tile_screen = []
-for i in range(SCREEN_TILE_WIDTH):
-    tile_screen.append([])
-    for j in range(SCREEN_TILE_HEIGHT):
-        tile_screen[i].append(missing_texture)
-
-
-def to_screen_space(coord):
-    return (coord[0] * TILE_WIDTH, coord[1] * TILE_HEIGHT)
-
-
-def to_screen_tile_space(choord):
-    return (int(choord[0] / TILE_WIDTH), int(choord[1] / TILE_HEIGHT))
-
-
-def draw_tiles():
-    for i in range(SCREEN_TILE_WIDTH):
-        for j in range(SCREEN_TILE_HEIGHT):
-            window.blit(tile_screen[i][j], to_screen_space((i, j)))
-
+for i in range(display.SCREEN_TILE_WIDTH):
+    display.tile_screen.append([])
+    for j in range(display.SCREEN_TILE_HEIGHT):
+        if i > 10:
+            texture = missing_texture
+        else:
+            texture = brown_texture
+        display.tile_screen[i].append(texture)
 
 while running:
     # poll for events
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-
     # fill the screen with a color to wipe away anything from last frame
-    window.fill("purple")
-    draw_tiles()
-    pygame.display.flip()
+    display.render()
     clock.tick(60)  # limits FPS to 60
 
 pygame.quit()
